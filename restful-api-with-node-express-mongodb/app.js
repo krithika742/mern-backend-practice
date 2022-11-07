d7f2dbfba67edf24d3f397d0fb170b2ae7888b39
krithika117 <krithikanithyanandam@gmail.com>
2022-11-07 15:19:39 +0530

    connection with db

--- restful-api-with-node-express-mongodb/app.js
+++ restful-api-with-node-express-mongodb/app.js
@@ -1,18 +1,26 @@
 const express = require('express')
 const app = express();
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 require('dotenv/config');
+
+app.use(bodyParser.json());
 
 //Import Routes
 const postsRoute = require('./routes/posts');
 app.use('/posts', postsRoute);
 
+//Home Route
 app.get('/', (req, res) => {
     res.send('Home!')
 })
 
 //Connect to DB
-mongoose.connect(process.env.DB_CONNECTION, {useNewUrlParser: true}, () => console.log('Connected to DB'))
+mongoose.connect(process.env.DB_CONNECTION, {
+    useNewUrlParser: true,
+    })   
+.then(() => console.log("Database connected!"))
+.catch(err => console.log(err));
 
 app.listen(5000, () => {
     console.log("Running")
--- restful-api-with-node-express-mongodb/routes/posts.js
+++ restful-api-with-node-express-mongodb/routes/posts.js
@@ -1,13 +1,26 @@
 const express = require('express');
-
 const router = express.Router();
+const Post = require('../models/Post');
 
 router.get('/', (req, res) => {
     res.send('Posts!');
 });
 
-router.get('/specific', (req, res) => {
-    res.send('Specific Post!');
+router.post('/', (req, res) => {
+    const post = new Post({
+        title: req.body.title,
+        description: req.body.description
+    });
+    post.save()
+        .then(data => {
+            res.json(data);
+            console.log(res.json(data));
+        })
+        .catch(err => {
+            res.json({message:err});
+            console.log(err);
+
+        });
 });
 
 module.exports = router;
