4d190ff613a99424e414fad2d2389ba22828a978
krithika117 <krithikanithyanandam@gmail.com>
2022-11-07 12:27:28 +0530

    initial commit

---
+++ basic-server/index.js
@@ -0,0 +1,25 @@
+const express = require("express")
+const app = express()
+const path = require("path")
+const port = 5000
+app.use(express.urlencoded({ extended: false }))
+app.use(express.static(path.join(__dirname, "public")))
+
+app.set("view engine", "ejs")
+app.set("views", path.join(__dirname, "views"))
+
+app.get('/', (req, res) => {
+    res.render("home")
+})
+
+app.get('/about', (req, res) => {
+    res.send(`<h1>About.</h1>`)
+})   
+
+app.get('/contact', (req, res) => {
+    res.send(`<h1>Contact.</h1>`)
+})
+
+app.listen(port, () => {
+    console.log('Listening')
+})
